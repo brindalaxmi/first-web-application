df54107f161dbc5848ff3b35c1cc22decf627775
brindalaxmi <160815855+brindalaxmi@users.noreply.github.com>
2024-02-27 11:35:40 +0000

    new website app

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'Location': 'Bengaluru,India',
+    'salary': '10lac'
+}, {
+    'id': 2,
+    'title': 'Scientist',
+    'Location': 'Bengaluru,India',
+    'salary': '10lac'
+}]
 
 
-@app.route("/")
-def Hello():
-  return "Hello all"
+@app.route('/')
+def Hello_ship():
+  return render_template('Dummy2.html', jobs=JOBS, company_name='Shipping')
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
